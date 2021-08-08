5e1c19591f7eb0304ce3f38cea196c1e620e8487
shinter61 <shinter61@gmail.com>
2021-08-08 17:30:59 +0900

    Initial Commit

---
+++ Nima/NimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NimaApp.swift
+//  Nima
+//
+//  Created by 松本真太朗 on 2021/08/08.
+//
+
+import SwiftUI
+
+@main
+struct NimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
