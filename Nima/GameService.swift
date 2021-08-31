75f8531e9b11db51d57258410ce581d493478556
shinter61 <shinter61@gmail.com>
2021-08-31 22:36:58 +0900

    Use glitch url

--- Nima/GameService.swift
+++ Nima/GameService.swift
@@ -9,7 +9,7 @@ import Foundation
 import SocketIO
 
 final class GameService: ObservableObject {
-    private var manager = SocketManager(socketURL: URL(string: "ws://localhost:3000")!, config: [.log(true), .compress])
+    private var manager = SocketManager(socketURL: URL(string: "https://hutarimajan.glitch.me/")!, config: [.log(true), .compress])
     
     @Published var socket: SocketIOClient!
     
